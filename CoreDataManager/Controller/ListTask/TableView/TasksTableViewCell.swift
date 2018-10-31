a75f8dc21608b653363a1fde5c75c4f8cda7689d
Tolga Taner <tolgataner43@gmail.com>
2018-10-31 16:06:18 +0300

    no message

--- CoreDataManager/Controller/ListTask/TableView/TasksTableViewCell.swift
+++ CoreDataManager/Controller/ListTask/TableView/TasksTableViewCell.swift
@@ -21,7 +21,7 @@ class TasksTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        // push check 
         // Configure the view for the selected state
     }
     
